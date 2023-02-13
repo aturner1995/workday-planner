b8b5b036efe95babf9747871e306298f2a860d69
Aaron <aaturner1995@gmail.com>
2023-02-13 14:04:27 -0400

    added message when you save to local storage, added clear button

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
   const hourTracker = () => {
     let currentHour = dayjs().hour();
     $('.time-block').each(function() {
-      let timeBlockHour = parseInt($(this).attr('id').split('hour')[1])
+      let timeBlockHour = parseInt($(this).attr('id').split('-')[1]);
       
       if (timeBlockHour === currentHour) {
         $(this).addClass('present');
@@ -26,44 +26,35 @@ $(document).ready(() => {
       }
     })    
   }
-const init = () => {
-  for (let i= 8; i <= 17; i++) {
-    $('#hour' + i + ' .description').val(localStorage.getItem('hour' + i));
+  // Init function to get tasks from local storage and set values
+  const init = () => {
+    for (let i= 8; i <= 17; i++) {
+      $('#hour-' + i + ' .description').val(localStorage.getItem('hour-' + i));
+    }
   }
-}
-
-
+  // Event listener for the save button to store element to local storage
   $('.saveBtn').on('click', function () {
     let task = $(this).siblings('.description').val();
     let timeBlock = $(this).parent().attr('id');
     localStorage.setItem(timeBlock,task);
-  }) 
-
-
+    $('.saved-message').text("Saved to local storage ✅");
+    setTimeout(hide,2000);
+  })
+  // Hide function to hide the saved message after 2 seconds
+  const hide = () => {
+    $(".saved-message").text("");
+  }
+  // Clear task list button
+  $('.clear-button').on('click', function() {
+    for (let i= 8; i <= 17; i++) {
+      let timeBlock = 'hour-' + i;
+      localStorage.removeItem(timeBlock);
+      $('#' + timeBlock + ' .description').val('');
+    }
+  })
 
   hourTracker();
   init();
   
 })
 
-
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
